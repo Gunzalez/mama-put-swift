e4399fddbb18e216f7ba0814c2bab0861ca87959
gunzalez <gunzalez@hotmail.com>
2022-12-10 11:51:57 +0000

    Initial Commit

---
+++ mama-put/mama_putApp.swift
@@ -0,0 +1,17 @@
+//
+//  mama_putApp.swift
+//  mama-put
+//
+//  Created by Segun Konibire on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct mama_putApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
