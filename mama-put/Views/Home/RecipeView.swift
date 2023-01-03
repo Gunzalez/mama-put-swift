ab2af62c9a5c07d289eeef848e0d701560590e0d
gunzalez <gunzalez@hotmail.com>
2023-01-03 20:46:16 +0000

    tries a new json format

---
+++ mama-put/Views/Home/RecipeView.swift
@@ -0,0 +1,31 @@
+//
+//  RecipeView.swift
+//  mama-put
+//
+//  Created by Segun Konibire on 31/12/2022.
+//
+
+import SwiftUI
+
+struct RecipeView: View {
+    
+    @State private var allrecipes = [Recipe]()
+    
+    var body: some View {
+        
+        VStack {
+            Text("Hello World again")
+            
+                        
+        }.onAppear {
+            allrecipes = JSONManager.load("recipes.json")
+            print("ContentView appeared!")
+        }
+    }
+}
+
+struct RecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeView()
+    }
+}
